ecfa9cc82a0d1484a315b1111203cec1b34f49ac
leonardo <leonardo.giannini@cern.ch>
2022-01-25 14:46:12 -0800

    review

--- python/postprocessing/examples/skimModule.py
+++ python/postprocessing/examples/skimModule.py
@@ -61,6 +61,8 @@ class vbsHwwSkimProducer_hadronic(Module):
         jets = Collection(event, "Jet")
         fatjets = Collection(event, "FatJet")
 
+ 
+
         charges_veto = []
         leptons_veto = []
         leptons_veto_jetIdx = []
@@ -98,16 +100,16 @@ class vbsHwwSkimProducer_hadronic(Module):
 
             isOverlap = False
             # Perform lepton - jet overlap removal
-            for lep in leptons_veto:
-                if ROOT.Math.VectorUtil.DeltaR(ROOT.nt.FatJet_p4()[i], lep) < 0.4:
-                    isOverlap = True
-                    break
+            #for lep in leptons_veto:
+            #    if ROOT.Math.VectorUtil.DeltaR(ROOT.nt.FatJet_p4()[i], lep) < 0.4:
+            #        isOverlap = True
+            #        break
 
             if isOverlap:
                 continue
 
             # Count jets with pt > 20
-            if fjet.pt > 250 and fjet.msoftdrop>40 and abs(fjet.eta)<2.5:
+            if fjet.pt >= 250 and fjet.msoftdrop>=40 and abs(fjet.eta)<=2.5 and fjet.jetId>0:
                 nfatjets += 1
                 fjets.append(ROOT.nt.FatJet_p4()[i])
 
@@ -122,14 +124,14 @@ class vbsHwwSkimProducer_hadronic(Module):
 
             # Perform lepton - jet overlap removal
             isOverlap = False
-            for ilep_jetidx in leptons_veto_jetIdx:
-                if i == ilep_jetidx:
-                    isOverlap = True
-                    break
+            #for ilep_jetidx in leptons_veto_jetIdx:
+            #    if i == ilep_jetidx:
+            #        isOverlap = True
+            #        break
 
             # Perform lepton - jet overlap removal
             for fjet in fjets:
-                if ROOT.Math.VectorUtil.DeltaR(ROOT.nt.Jet_p4()[i], fjet) < 0.4:
+                if ROOT.Math.VectorUtil.DeltaR(ROOT.nt.Jet_p4()[i], fjet) < 0.8:
                     isOverlap = True
                     break
 
@@ -155,6 +157,7 @@ class vbsHwwSkimProducer_hadronic(Module):
         mjj_method3=-1
         deta_method3=-1
         
+        o,p=-1,-1
         for index1 in indicesVBF:
             for index2 in indicesVBF:
               if (index1==index2): 
@@ -165,6 +168,8 @@ class vbsHwwSkimProducer_hadronic(Module):
               if mjj>mjj_mjj_max:
                 mjj_mjj_max=mjj
                 deta_mjj_max=deta
+                o=index1
+                p=index2
               
               if (deta>deta_deta_max):
                 deta_deta_max=deta
@@ -187,7 +192,7 @@ class vbsHwwSkimProducer_hadronic(Module):
             if(jets[index1].eta*jets[Imax].eta<0):
               if(ROOT.nt.Jet_p4()[index1].E()>Emax2):
                 Emax2=ROOT.nt.Jet_p4()[index1].E()
-                Imax2=index2
+                Imax2=index1
                 
         if(Imax>0 and Imax2>0):
           mjj_method3=(ROOT.nt.Jet_p4()[Imax]+ROOT.nt.Jet_p4()[Imax2]).M()
@@ -202,7 +207,26 @@ class vbsHwwSkimProducer_hadronic(Module):
         VBF2=(mjj_deta_max>500 and deta_deta_max>3.0)
         VBF3=(mjj_method3>500 and deta_method3>3.0)
         VBF_OR = (VBF1 or VBF2 or VBF3)
-                
+
+        '''passo=True
+        if (nelectrons_veto + nmuons_veto >= 1):
+          passo=False
+        if (nfatjets<1):
+          passo=False
+        if (nfatjets==1 and not VBF_OR):
+         passo=False
+        if (nfatjets==2 and not VBF_OR):
+         passo=False
+        if (nfatjets>=3 and njets_VBF==0):
+         passo=False
+        passo=int(passo)'''    
+        #print "event number: %s pass or not: %s nleptons: %s njets: %s nfatjets: %s VBF max mass: %s VBF max deltaEta: %s" %(event._entry, passo, nelectrons_veto + nmuons_veto, njets_VBF, nfatjets, mjj_method3, deta_method3)
+        #print "event number: %s pass or not: %s nleptons: %s njets: %s nfatjets: %s VBF max mass: %s VBF max deltaEta: %s" %(event._entry, passo, nelectrons_veto + nmuons_veto, njets_VBF, nfatjets, mjj_mjj_max, deta_mjj_max)
+        #print "event number: %s pass or not: %s nleptons: %s njets: %s nfatjets: %s VBF max mass: %s VBF max deltaEta: %s" %(event._entry, passo, nelectrons_veto + nmuons_veto, njets_VBF, nfatjets, mjj_deta_max, deta_deta_max)  
+        #for index1 in indicesVBF: 
+        #    f1=int((index1==o) or (index1==p))
+        #    print "VBF Jet flag: %s Jet_mass: %s Jet_Pt:%s Jet_Eta:%s Jet_Phi: %s" %( f1, jets[index1].mass, jets[index1].pt, jets[index1].eta, jets[index1].phi )              
+ 
         if (nelectrons_veto + nmuons_veto >= 1):
           return False
         if (nfatjets<1):
